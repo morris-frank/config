710cf3243f1c7e46613bb6b714bc22255b3e3535
Tukkin Moriarty <mrtukkin@gmail.com>
2016-02-18 23:03:38 +0100

    Changed names and added workspace moving commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,65 +15,75 @@ PWD=$(pwd)
 ###
 
 ###
+#CHECK FOR MISSING COMMANDS
+###
+	for p in wmctrl xprop xdotool ls++ zathura xboomx xbacklight amixer; do
+		if ! type "$p" &> /dev/null; then
+			echo "$p is missing... That will break stuff"
+		fi
+	done
+###
+
+###
 #CONFIG FILES
 ###
 	#XRESOURCES
-	ln -svf "$PWD/Xresources"  "$HOME/.Xresources"
-	type -f "$PWD/Xresources_extra" &> /dev/null && ln -svf "$PWD/Xresources_extra"  "$HOME/.Xresources_extra"
+	ln -sf "$PWD/Xresources"  "$HOME/.Xresources"
+	type -f "$PWD/Xresources_extra" &> /dev/null && ln -sf "$PWD/Xresources_extra"  "$HOME/.Xresources_extra"
 	
 	#BASHRC
-	ln -svf "$PWD/bashrc"  "$HOME/.bashrc"
+	ln -sf "$PWD/bash.rc"  "$HOME/.bashrc"
 	
 	#BASH_PROFILE
-	ln -svf "$PWD/bash_profile"  "$HOME/.bash_profile"
+	ln -sf "$PWD/bash_profile"  "$HOME/.bash_profile"
 	
-	#INPUTRC
-	ln -svf "$PWD/inputrc"  "$HOME/.inputrc"
+ 	#INPUTRC
+	ln -sf "$PWD/input.rc"  "$HOME/.inputrc"
 	
 	#NANORC
-	ln -svf "$PWD/nanorc"  "$HOME/.nanorc"
+	ln -sf "$PWD/nano.rc"  "$HOME/.nanorc"
 	
 	#SPECTRWM
-	ln -svf "$PWD/spectrwmrc"  "$HOME/.spectrwm.conf"
-	ln -svf "$PWD/spectrwm_de.conf"  "$HOME/.spectrwm_de.conf"
+	ln -sf "$PWD/spectrwm.rc"  "$HOME/.spectrwm.conf"
+	ln -sf "$PWD/spectrwm_de.rc"  "$HOME/.spectrwm_de.conf"
 	
 	#XBINDKEYSRC
-	ln -svf "$PWD/xbindkeysrc"  "$HOME/.xbindkeysrc"
+	ln -sf "$PWD/xbindkeys.rc"  "$HOME/.xbindkeysrc"
 	
 	#XINITRC
-	ln -svf "$PWD/xinitrc"  "$HOME/.xinitrc"
+	ln -sf "$PWD/xinit.rc"  "$HOME/.xinitrc"
 	
 	#ZSH
-	ln -svf "$PWD/zshrc"  "$HOME/.zshrc"
+	ln -sf "$PWD/zsh.rc"  "$HOME/.zshrc"
 	
 	#EMACS
 	mkdir "$HOME/.emacs.d" 2> /dev/null
-	ln -svf "$PWD/emacsrc" "$HOME/.emacs.d/init.el"
+	ln -sf "$PWD/emacs.rc" "$HOME/.emacs.d/init.el"
 	
 	#NCMPCPP
 	mkdir "$HOME/.ncmpcpp" 2> /dev/null
-	ln -svf "$PWD/ncmpcpprc" "$HOME/.ncmpcpp/config"
+	ln -sf "$PWD/ncmpcpp.rc" "$HOME/.ncmpcpp/config"
 	
 	#XBOOMX
 	mkdir "$HOME/.xboomx" 2> /dev/null
-	ln -svf "$PWD/xboomxrc" "$HOME/.xboomx/config"
+	ln -sf "$PWD/xboomx.rc" "$HOME/.xboomx/config"
 	
 	#TODO.SH
 	mkdir "$HOME/.todo" 2> /dev/null
-	ln -svf "$PWD/todorc" "$HOME/.todo/config"
+	ln -sf "$PWD/todo.rc" "$HOME/.todo/config"
 	
 	#COMPTON
-	ln -svf "$PWD/comptonrc" "$HOME/.compton.conf"
+	ln -sf "$PWD/compton.rc" "$HOME/.compton.conf"
 	
 	#SUBLIME TEST 3
 	mkdir -p "$HOME/.config/sublime-text-3/Packages/User" 2> /dev/null
-	ln -svf "$PWD/Preferences.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/"
+	ln -sf "$PWD/Preferences.sublime-settings" "$HOME/.config/sublime-text-3/Packages/User/"
 ###
 
 ###
 #SCRIPTS
 ###
 	mkdir "$HOME/bin" 2> /dev/null
-	ln -svf "$PWD/bar" "$HOME/bin/bar"
-	ln -svf "$PWD/bar.content.sh" "$HOME/bin/bar.content.sh"
+	ln -sf "$PWD/commands/ws_next_used" "$HOME/bin/"
+	ln -sf "$PWD/commands/ws_prev_used" "$HOME/bin/"
 ###
