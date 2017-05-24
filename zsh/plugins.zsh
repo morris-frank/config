840e25bd816ba777796fe4de7070133ff08212ff
Maurice Frank <maurice.frank@posteo.de>
2017-05-24 18:37:48 +0200

    Various updates

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -6,12 +6,12 @@ zplug mafredri/zsh-async, from:github
 zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
 
 # Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
-fi
+#if ! zplug check --verbose; then
+#    printf "Install? [y/N]: "
+#    if read -q; then
+#        echo; zplug install
+#    fi
+#fi
 
 # Then, source plugins and add commands to $PATH
 zplug load
