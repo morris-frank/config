70c7b5eb8749ae4b34f29a9da4e490a6ffe976bb
Maurice Frank <maurice.frank@posteo.de>
2018-05-05 13:17:16 +0200

    removed blink from alacritty and added remote script

---
+++ remote.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+cd $(dirname "$0")
+PWD=$(pwd)
+
+source "$PWD/shell.rc"
+
+#CONFIG FILES
+###
+	echo "${GRE}[bootstrap.sh] Link all config files to their correct positions${RES}"
+	#shells
+	ln -sf "$PWD/shell.rc"  "$HOME/.shellrc"
+
+	#bash
+	ln -sf "$PWD/bash.rc"  "$HOME/.bashrc"
+
+	#profile
+	ln -sf "$PWD/profile"  "$HOME/.profile"
+
+ 	#input
+	ln -sf "$PWD/input.rc"  "$HOME/.inputrc"
+
+	#nano
+	ln -sf "$PWD/nano.rc"  "$HOME/.nanorc"
+
+	#git
+	ln -sf "$PWD/git.rc" "$HOME/.gitconfig"
+
+	#tmux
+	ln -sf "$PWD/tmux/tmux.rc" "$HOME/.tmux.conf"
+###
+
+# Setting up ZSH
+###
+  if type zsh &> /dev/null; then
+		echo "${GRE}[bootstrap.sh] Setting up ZSH${RES}"
+		ln -sf "$PWD/zsh.rc"  "$HOME/.zshrc"
+		rm -rf $HOME/.zplug
+		curl -sL --proto-redir -all,https https://zplug.sh/installer | zsh
+  fi
+###
