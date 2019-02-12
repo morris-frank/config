ff790bb44782e4260a24388d3e9e2f0ee6a9d8f5
Maurice Frank <maurice.frank@posteo.de>
2019-02-12 13:53:40 +0100

    updated zsh wordchars

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -7,7 +7,7 @@ bindkey '^K' history-substring-search-up
 bindkey '^J' history-substring-search-down
 
 default-backward-delete-word () {
-  local WORDCHARS="*?_[]~=/&;!#$%^(){}<>"
+  local WORDCHARS="*?_[]~=&;!#$%^(){}<>"
   zle backward-delete-word
 }
 zle -N default-backward-delete-word
